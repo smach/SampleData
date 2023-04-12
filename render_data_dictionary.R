e67bba3431a71550da505af1c1f08967f7a114f9
SharonMachlis <smachlis@computerworld.com>
2023-04-11 20:26:16 -0400

    Data dictionary update fix lack of link and other small things

--- render_data_dictionary.R
+++ render_data_dictionary.R
@@ -1,9 +1,13 @@
 system('quarto render')
 system('cp -r www docs')
 system('git rm --cached data_dictionary_table_data.Rds')
+system('git rm --cached -r www')
+system('git rm --cached -r docs/www')
 system('git add data_dictionary_table_data.Rds')
+system('git add www/*')
+system('git add docs/www/(')
 system('git add *')
-system('git commit -m "Data dictionary update with links not opening to new tab"')
+system('git commit -m "Data dictionary update fix lack of link and other small things"')
 system('git push')
 
 # If I want to view the site
